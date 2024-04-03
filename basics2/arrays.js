1e6f1694ac973899fdc040cde4f6417d55ba8c73
ani3321r <ani3321r@gmail.com>
2024-04-03 21:37:34 +0530

    arrays

---
+++ basics2/arrays.js
@@ -0,0 +1,11 @@
+var porsches = [];
+var horsepower = []; //another way of declaring a array 
+var allUser = []; //for a array of user defined type
+var models = [
+    [201, 521, 632],
+    [841, 852, 741],
+    []
+]; //for nested arrays
+porsches.push("918 spyder");
+horsepower.push(750);
+allUser.push({ name: "", isVerified: false });
---
+++ basics2/arrays.ts
@@ -0,0 +1,22 @@
+const porsches: string[] = []
+
+const horsepower: Array<number> =[] //another way of declaring a array 
+
+type User = {
+    name: string
+    isVerified: boolean
+}
+
+const allUser: User[] = []//for a array of user defined type
+
+const models: number[][] = [
+    [201, 521, 632],
+    [841, 852, 741],
+    []
+]//for nested arrays
+
+
+porsches.push("918 spyder")
+horsepower.push(750)
+
+allUser.push({name: "", isVerified: false})
