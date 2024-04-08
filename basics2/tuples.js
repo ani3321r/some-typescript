2175fae80fac67d24f64d4f85b91a8e1c427d3ed
ani3321r <ani3321r@gmail.com>
2024-04-08 21:55:45 +0530

    tuples

---
+++ basics2/tuples.js
@@ -0,0 +1,6 @@
+//tuples are mainly used to maintain the order
+var tUser;
+tUser = [63, "abc", false];
+var rgb;
+var newUser = [89, "abc@ai.com"];
+newUser[1] = "bcd@ai.com"; //this is how we can modify the values, i.e, using indexes are valid
---
+++ basics2/tuples.ts
@@ -0,0 +1,13 @@
+//tuples are mainly used to maintain the order
+
+let tUser: [number, string, boolean]
+
+tUser = [63, "abc", false];
+
+let rgb: [number, number, number]
+
+type User =[number, string]
+
+const newUser: User = [89, "abc@ai.com"]
+
+newUser[1] = "bcd@ai.com"//this is how we can modify the values, i.e, using indexes are valid
